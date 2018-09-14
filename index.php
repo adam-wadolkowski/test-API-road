8224d2374b9ee4b923d3a7e01f94534e04c7243d
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2018-09-14 22:12:25 +0200

    Added tested functionality class

--- index.php
+++ index.php
@@ -5,3 +5,12 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
+
+use App\Models\Provider\Provider;
+
+
+$urlOptions = ['from' => 'Denver%2C+CO','to' => 'Boulder%2C+CO','outFormat' => 'json'];
+
+$provider = new Provider($urlOptions);
+var_dump($provider);
+//$transitData = $provider->getTransitData();
