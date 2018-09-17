9aab8fe9d05e28ad39fefde1e93e038ea64ab8ab
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2018-09-17 19:04:43 +0200

    Modified index

--- index.php
+++ index.php
@@ -7,10 +7,12 @@ error_reporting(E_ALL);
 require_once('vendor/autoload.php');
 
 use App\Models\Provider\Provider;
+use App\Controllers\Transit\Transits;
 
 
-$urlOptions = ['from' => 'Denver%2C+CO','to' => 'Boulder%2C+CO','outFormat' => 'json'];
+$urlOptions = ['from' => 'Denver%2C+CO', 'to' => 'Boulder%2C+CO','outFormat' => 'json','routeType' =>'shortest', 'unit' => 'k' ];
 
 $provider = new Provider($urlOptions);
-var_dump($provider);
-//$transitData = $provider->getTransitData();
+$providerData = $provider->getTransitData();
+//var_dump($providerData);
+$transits = new Transits();
